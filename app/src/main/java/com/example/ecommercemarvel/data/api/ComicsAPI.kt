5da0c49db61c877f5263daf6847d6ff97566c14e
Josebias Neto <josebiasbsneto@gmail.com>
2021-09-02 14:52:38 -0300

    Trying pass the params manuality

--- app/src/main/java/com/example/ecommercemarvel/data/api/ComicsAPI.kt
+++ app/src/main/java/com/example/ecommercemarvel/data/api/ComicsAPI.kt
@@ -12,13 +12,14 @@ interface ComicsAPI {
     suspend fun getComics(
         @Query("apikey") apikey: String,
         @Query("ts") timestamp: String,
-        @Query("hash") input: String
+        @Query("hash") hash: String
     ): ParentAPI
 
     object getHTTPRequest{
         const val PUBLIC_KEY = "f3d31a9d162e766cc8317a4dcc3af5cb"
         const val PRIVATE_KEY = "d9062e679217e8957884d76b9f8360580edb5126"
-        val ts: Long = System.currentTimeMillis()/1000
+        val ts = 1630534375
+        //val ts: Long = System.currentTimeMillis()/1000
         val input: String = ts.toString() + PRIVATE_KEY + PUBLIC_KEY
 
         fun md5Hahs(input: String) : String{
--- app/src/main/java/com/example/ecommercemarvel/data/repository/ComicsAPIDatasource.kt
+++ app/src/main/java/com/example/ecommercemarvel/data/repository/ComicsAPIDatasource.kt
@@ -5,8 +5,8 @@ import com.example.ecommercemarvel.data.api.ComicsAPI
 class ComicsAPIDatasource(private val comicsAPI: ComicsAPI) {
 
     suspend fun getComics() = comicsAPI.getComics(
-        ComicsAPI.getHTTPRequest.PUBLIC_KEY, ComicsAPI.getHTTPRequest.ts.toString(),
-        ComicsAPI.getHTTPRequest.output
+        "f3d31a9d162e766cc8317a4dcc3af5cb", ComicsAPI.getHTTPRequest.ts.toString(),
+        "2fcb2e5d735889078f6508701aa3ef7c"
     )
 
 }
